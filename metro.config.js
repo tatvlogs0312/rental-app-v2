32d5491ab8cd7233d214fe3454c50b6f47d98fe5
obito <tanhtuan093@gmail.com>
2024-10-26 19:30:18 +0700

    update

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,23 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig: getDefaultExpoConfig } = require('expo/metro-config');
+const { mergeConfig, getDefaultConfig } = require('@react-native/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
+const expoConfig = getDefaultExpoConfig(__dirname);
 const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
+const { assetExts, sourceExts } = expoConfig.resolver;
+
 const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer/react-native"),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"],
-  },
+    transformer: {
+        ...expoConfig.transformer,
+        babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
+    },
+    resolver: {
+        ...expoConfig.resolver,
+        assetExts: assetExts.filter((ext) => ext !== "svg"),
+        sourceExts: [...sourceExts, "svg"]
+    }
 };
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = mergeConfig(defaultConfig, expoConfig, config);
